9acd531578a9f87c19a1222107ca1a15b9a75e8e
stefan-kanazir <kanazir.stefan@gmail.com>
2021-12-08 23:14:48 +0100

    Displaying List component on the FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import List from "./components/List"
 
@@ -14,7 +13,14 @@ interface IState {
 
 function App() {
 
-  const [food, setFood] = useState<IState["food"]>([])
+  const [food, setFood] = useState<IState["food"]>([
+    {
+      title: "Eggs",
+      url: "https://images.unsplash.com/photo-1582169505937-b9992bd01ed9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1410&q=80",
+      calories: 10,
+      description: "Egg yolks and whole eggs store significant amounts of protein and choline"
+    }
+  ])
 
   return (
     <div className="App">
@@ -22,7 +28,7 @@ function App() {
       <p>Recording your calories as you eat gives you a clearer picture of the calories you are consuming. It also enhances your awareness of food values and nutrient levels, all of which helps you make healthier choices when eating.</p>
       
       <section>
-        <List food={food} />
+        <List food={food}/>
       </section>
     </div>
   );
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
 
 interface IProps {
-    food: {
-      title: string
-      url: string
-      calories: number
-      description?: string
-    }[]
-  }
-
-function List(props: IProps) {
-    return (
-        <div>
-            
-        </div>
-    )
+  food: {
+    title: string
+    url: string
+    calories: number
+    description?: string
+  }[]
 }
 
-export default List
+const List: React.FC<IProps> = ({food}) => {
+
+  const renderList = (): JSX.Element[] => {
+    return food.map(food => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={food.url} alt={food.title} />
+            <h2>{food.title}</h2>
+          </div>
+          <p>{food.calories} cal</p>
+          <p className="List-note">{food.description}</p>
+        </li>
+      )
+    })
+  }
+
+  return (
+    <ul>
+      {renderList()}
+    </ul>
+  )
+}
+
+export default List;
